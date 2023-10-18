c0b80a0c0abae5b72176cb6ef2cf6791324f64e2
Tristan Jahnke <trjahnke@gmail.com>
2023-10-18 16:08:24 -0500

    add link to github

--- test/phoenix_live_light_web/controllers/page_controller_test.exs
+++ test/phoenix_live_light_web/controllers/page_controller_test.exs
@@ -3,6 +3,5 @@ defmodule PhoenixLiveLightWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
   end
 end
